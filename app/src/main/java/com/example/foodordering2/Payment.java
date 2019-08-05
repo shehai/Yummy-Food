e94dbfd5c9877460dac398c704e51929fe0209fe
dileepapavel <IT18107210@my.sliit.lk>
2019-08-05 16:05:01 +0530

    designed a feedback

---
+++ app/src/main/java/com/example/foodordering2/Payment.java
@@ -0,0 +1,24 @@
+package com.example.foodordering2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Payment extends AppCompatActivity {
+
+    TextView textView6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+        textView6 = findViewById(R.id.textView);
+    }
+
+    public void startnewActivity(View view){
+        Intent intent = new Intent(Payment.this,Feedback.class);
+        startActivity(intent);
+    }
+}
